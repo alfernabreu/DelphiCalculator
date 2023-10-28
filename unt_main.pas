d418019bc4d33fea9f937fc3de62872f01f0d3a5
alfernab <86300297+alfernabreu@users.noreply.github.com>
2023-10-28 19:46:12 -0300

    add first files from project

---
+++ unt_main.pas
@@ -0,0 +1,24 @@
+unit unt_main;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+
+type
+  Tfrm_main = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frm_main: Tfrm_main;
+
+implementation
+
+{$R *.fmx}
+
+end.
