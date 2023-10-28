bc145a6fad7461de5edf3a51824cb3bc7658ebc1
alfernab <86300297+alfernabreu@users.noreply.github.com>
2023-10-28 20:31:03 -0300

    add buttons

--- unt_main.pas
+++ unt_main.pas
@@ -4,10 +4,29 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls;
 
 type
   Tfrm_main = class(TForm)
+    btn_0: TButton;
+    btn_1: TButton;
+    btn_2: TButton;
+    btn_3: TButton;
+    btn_4: TButton;
+    btn_5: TButton;
+    btn_6: TButton;
+    btn_7: TButton;
+    btn_8: TButton;
+    btn_9: TButton;
+    btn_point: TButton;
+    btn_result: TButton;
+    btn_plusMinus: TButton;
+    btn_som: TButton;
+    btn_subtract: TButton;
+    btn_division: TButton;
+    btn_multiplier: TButton;
+    btn_sqrt: TButton;
   private
     { Private declarations }
   public
